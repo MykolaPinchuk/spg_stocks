430d063d1ceded577f7f8d4bf768660690ebb7a5
Mykola Pinchuk <pinchukmykola@gmail.com>
2022-12-02 02:56:27 +0000

    added .py script, the first working model, 1m-2m freq, emxc target

--- .ipynb_checkpoints/modeling-checkpoint.py
+++ .ipynb_checkpoints/modeling-checkpoint.py
@@ -8,7 +8,7 @@ import yfinance as yf
 
 from sklearn.compose import ColumnTransformer, make_column_transformer
 from sklearn.preprocessing import LabelBinarizer, LabelEncoder, OrdinalEncoder, OneHotEncoder, StandardScaler
-from sklearn.linear_model import LogisticRegression, LinearRegression, Lasso, Ridge
+from sklearn.linear_model import LinearRegression, Lasso, Ridge, ElasticNet
 from sklearn.model_selection import RandomizedSearchCV, GridSearchCV, train_test_split, KFold
 from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier, VotingClassifier
 from sklearn.metrics import accuracy_score, f1_score, r2_score, mean_squared_error
@@ -25,10 +25,15 @@ time0 = time.time()
 os.chdir('/home/jupyter/projects_gcp_cpu/spx/src')
 os.getcwd()
 
-tickerStrings = ['^GSPC', '^IXIC', '^RUT', 'EEM']
+tickerStrings = ['^GSPC', '^IXIC', '^RUT', 'EEM', 'EMXC']
 df_list = list()
 for ticker in tickerStrings:
-    data = yf.download(ticker, group_by="Ticker", period='60d', interval='30m', prepost=False, auto_adjust=True)
+    data = yf.download(ticker, 
+                       group_by="Ticker", 
+                       period='7d', 
+                       interval='1m', 
+                       prepost=False, 
+                       auto_adjust=True)
     data['ticker'] = ticker  
     df_list.append(data)
 
@@ -37,57 +42,113 @@ df = df[['Close', 'ticker']]
 df.replace({'^GSPC':'Spx', '^IXIC':'Nasdaq', '^RUT':'Russel'}, inplace=True)
 df = (df.pivot_table(index=['Datetime'], columns='ticker', values='Close'))
 
-df.columns = ['EEM', 'Nasdaq', 'Russel', 'Spx']
+df.columns = ['EEM', 'EMXC', 'Nasdaq', 'Russel', 'Spx']
 df['time'] = df.index.time
 df['date'] = df.index.date
 
 df = df.fillna(method='ffill')
-dayclose = df[df.time==datetime.time(15, 30, 0)]
+dayclose = df[df.time==datetime.time(15, 59, 0)]
 dayopen = df[df.time==datetime.time(9, 30, 0)]
 dayopen.reset_index(drop=True, inplace=True)
 dayclose.reset_index(drop=True, inplace=True)
 dayclose.sort_values(by='date')
 display(df, dayopen.head(), dayclose.head())
-
-df
+df0 = df.copy()
 
 
 ### now i wanna do feature engineering for all assets 
 
+asset_list = ['Spx', 'Nasdaq', 'Russel', 'EEM', 'EMXC']
 
-asset = 'Spx'
+for asset in asset_list:
+    
+    df[asset + '_ret'] = 100*(df[asset]/df[asset].shift(1)-1)
+    df['s_' + asset + '_ret_1prd'] = (100*(df[asset]/df[asset].shift(1)-1)).shift(1)
+    df['s_' + asset + '_ret_2prd'] = (100*(df[asset]/df[asset].shift(2)-1)).shift(1)
+    df['s_' + asset + '_ret_4prd'] = (100*(df[asset]/df[asset].shift(4)-1)).shift(1)
+    display(df.shape, df.head(5))
+
+    df.loc[df.time < datetime.time(9, 32, 0), 's_' + asset + '_1prd'] = np.nan
+    df.loc[df.time < datetime.time(9, 33, 0), 's_' + asset + '_2prd'] = np.nan
+    df.loc[df.time < datetime.time(9, 35, 0), 's_' + asset + '_4prd'] = np.nan
+
+    dayopen.rename(columns={asset:asset+'_open'}, inplace=True)
+    dayopen.head()
+    dayclose.rename(columns={asset:asset+'_close'}, inplace=True)
+    dayclose_l1 = dayclose.copy()
+    dayclose_l2 = dayclose.copy()
+    dayclose_l1[asset+'_close_l1'] = dayclose_l1[asset+'_close'].shift(1)
+    dayclose_l2[asset+'_close_l2'] = dayclose_l2[asset+'_close'].shift(2)
+
+    # display(dayclose_l1.head(), dayclose_l2.head())
+
+    df = pd.merge(df, dayopen[['date', asset + '_open']], on=['date'], how='left')
+    df = pd.merge(df, dayclose_l1[['date', asset + '_close_l1']], on=['date'], how='left')
+    df = pd.merge(df, dayclose_l2[['date', asset + '_close_l2']], on=['date'], how='left')
+
+    df['s_' + asset + '_ret_open'] = (100*(df[asset]/df[asset + '_open']-1)).shift(1)
+    df['s_' + asset + '_ret_close1'] = (100*(df[asset]/df[asset + '_close_l1']-1)).shift(1)
+    df['s_' + asset + '_ret_close2'] = (100*(df[asset]/df[asset + '_close_l2']-1)).shift(1)
+
+    cols_todrop = [x for x in list(df.columns) if asset in x and 'ret' not in x]
+    df.drop(columns = cols_todrop, inplace=True)
+
+display(time.time() - time0, df.head())
+
+### do modeling ###
+
+t_df = df.copy()
+t_df.rename(columns={'EMXC_ret':'target'}, inplace=True)
+t_df.drop(columns = ['time', 'date', 'Spx_ret', 'Nasdaq_ret', 'Russel_ret', 'EEM_ret'], 
+          inplace=True)
+t_df
+
+t_df = t_df.dropna()
+t_df.info()
 
 
-df[asset + '_ret'] = 100*(df[asset]/df[asset].shift(1)-1)
-df['s_' + asset + '_ret_30m'] = (100*(df[asset]/df[asset].shift(1)-1)).shift(1)
-df['s_' + asset + '_ret_1h'] = (100*(df[asset]/df[asset].shift(2)-1)).shift(1)
-df['s_' + asset + '_ret_2h'] = (100*(df[asset]/df[asset].shift(4)-1)).shift(1)
-display(df.shape, df.head(5))
+y = t_df.pop('target')
+X = t_df
 
-df.loc[df.time < datetime.time(10, 0, 0), 's_' + asset + '_ret'] = np.nan
-df.loc[df.time < datetime.time(10, 30, 0), 's_' + asset + '_30m'] = np.nan
-df.loc[df.time < datetime.time(11, 0, 0), 's_' + asset + '_1h'] = np.nan
-df.loc[df.time < datetime.time(12, 0, 0), 's_' + asset + '_2h'] = np.nan
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=int(0.2*X.shape[0]))
+display(X_train.shape, X_test.shape, y_train.shape, X_train.head())
 
-dayopen.rename(columns={asset:asset+'_open'}, inplace=True)
-dayopen.head()
+time1 = time.time()
 
-dayclose.rename(columns={asset:asset+'_close'}, inplace=True)
-dayclose_l1 = dayclose.copy()
-dayclose_l2 = dayclose.copy()
+xgbm = XGBRegressor()
+parameters = {'eta':[0.03, 0.04, 0.05, 0.06, 0.07], 
+              'max_depth':[2, 3],
+             'subsample':[0.8, 1],
+             'colsample_bytree':[0.6, 1]}
+xgbgs = GridSearchCV(xgbm, parameters, cv=2)
+xgbgs.fit(X_train, y_train)
+print(xgbgs.best_params_)
 
-dayclose_l1[asset+'_close_l1'] = dayclose_l1[asset+'_close'].shift(1)
-dayclose_l2[asset+'_close_l2'] = dayclose_l2[asset+'_close'].shift(2)
+rdm = ElasticNet()
+parameters = {'alpha':[0, 0.001, 0.01, 0.05, 0.1, 0.2], 
+              'l1_ratio':[0, 0.2, 0.5, 0.8, 1]}
+rmgs = GridSearchCV(rdm, parameters, scoring='r2', cv=4)
+rmgs.fit(X_train, y_train)
+print(rmgs.best_params_)
 
-display(dayclose_l1.head(), dayclose_l2.head())
+print('In sample, xgb: ', r2_score(y_train, xgbgs.predict(X_train)))
+print('Out of sample, xgb: ', r2_score(y_test, xgbgs.predict(X_test)))
 
-df = pd.merge(df, dayopen[['date', asset + '_open']], on=['date'], how='left')
-df = pd.merge(df, dayclose_l1[['date', asset + '_close_l1']], on=['date'], how='left')
-df = pd.merge(df, dayclose_l2[['date', asset + '_close_l2']], on=['date'], how='left')
+print('In sample, ridge: ', r2_score(y_train, rmgs.predict(X_train)))
+print('Out of sample, ridge: ', r2_score(y_test, rmgs.predict(X_test)))
 
-df['s_' + asset + '_ret_open'] = (100*(df[asset]/df[asset + '_open']-1)).shift(1)
-df['s_' + asset + '_ret_close1'] = (100*(df[asset]/df[asset + '_close_l1']-1)).shift(1)
-df['s_' + asset + '_ret_close2'] = (100*(df[asset]/df[asset + '_close_l2']-1)).shift(1)
+print('Total time: ', time.time()-time0)
 
-cols_todrop = [x for x in list(df.columns) if asset in x and 'ret' not in x]
-df.drop(columns = cols_todrop, inplace=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
