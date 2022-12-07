455af432db3f660afa89cba173a19e158ef97f53
Mykola Pinchuk <pinchukmykola@gmail.com>
2022-12-07 14:14:23 +0000

    updated file for model eval, will do cf next

--- .ipynb_checkpoints/daily_perf_eval-checkpoint.py
+++ .ipynb_checkpoints/daily_perf_eval-checkpoint.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-import os, time, warnings, random, shap, requests, optuna, datetime
+import os, time, warnings, random, shap, requests, optuna, datetime, joblib, pytz
 import seaborn as sns
 import matplotlib.pyplot as plt
 import functools as ft
@@ -11,7 +11,7 @@ from sklearn.preprocessing import LabelBinarizer, LabelEncoder, OrdinalEncoder, 
 from sklearn.linear_model import LinearRegression, Lasso, Ridge, ElasticNet
 from sklearn.model_selection import RandomizedSearchCV, GridSearchCV, train_test_split, KFold
 from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier, VotingClassifier
-from sklearn.metrics import accuracy_score, f1_score, r2_score, mean_squared_error
+from sklearn.metrics import r2_score, mean_squared_error
 from sklearn.inspection import permutation_importance
 from xgboost import XGBClassifier, XGBRegressor
 
@@ -22,29 +22,44 @@ warnings.filterwarnings('ignore')
 
 time0 = time.time()
 
-os.chdir('/home/jupyter/projects_gcp_cpu/spx/src')
-os.getcwd()
+bucket_path = 'gs://pmykola-streaming-projects/spg-stocks/data/'
 
-tickerStrings = ['^GSPC', '^IXIC', '^RUT', 'EEM', 'EMXC', 'EEMA', 'VTHR']
-df_list = list()
-for ticker in tickerStrings:
-    data = yf.download(ticker, 
-                       group_by="Ticker", 
-                       period='60d', 
-                       interval='2m', 
-                       prepost=False, 
-                       auto_adjust=True)
-    data['ticker'] = ticker  
-    df_list.append(data)
+pull_time = datetime.datetime.now()
+pull_time = pull_time.astimezone(pytz.timezone('America/New_York'))
+pull_time = pull_time.replace(tzinfo=None)
+now_time = (str(pull_time.month) + '_' + 
+str(pull_time.day) + '_' +
+str(pull_time.hour) + ':'  +
+str(pull_time.minute) + ':' +
+str(pull_time.second))
 
-df = pd.concat(df_list)
-df = df[['Close', 'ticker']]
-df.replace({'^GSPC':'Spx', '^IXIC':'Nasdaq', '^RUT':'Russel'}, inplace=True)
-df = (df.pivot_table(index=['Datetime'], columns='ticker', values='Close'))
-df.columns = ['EEM', 'EEMA', 'EMXC', 'Nasdaq', 'Russel', 'Spx', 'VTHR']
+datafiles = !gsutil ls gs://pmykola-streaming-projects/spg-stocks/data
+start_file = [x for x in datafiles if ('data_start_' in x)]
+datafiles = [x for x in datafiles if ('auto_data_last_' in x) & ('pull_time' in x)]
+assert len(start_file) == 1
+start_file = start_file[0]
 
-df['time'] = df.index.time
-df['date'] = df.index.date
+df = pd.read_csv(start_file)
+df.Datetime = pd.to_datetime(df.Datetime)
+
+df_new = pd.DataFrame(columns = df.columns)
+for file in datafiles:
+    temp_df = pd.read_csv(file)
+    df_new = pd.concat([df_new, temp_df], axis=0)
+    # remove duplicates
+df_new.reset_index(inplace=True, drop=True)
+df_new.drop_duplicates(inplace=True)
+df_new.Datetime = pd.to_datetime(df_new.Datetime)
+df_new.sort_values(by='Datetime')
+
+df = pd.read_csv(start_file)
+df.Datetime = pd.to_datetime(df.Datetime)
+df = pd.concat([df, df_new])
+df.drop_duplicates(inplace=True)
+df.sort_values(by='Datetime')
+
+df['time'] = df.Datetime.dt.time
+df['date'] = df.Datetime.dt.date
 
 df = df.fillna(method='ffill')
 dayclose = df[df.time==datetime.time(15, 58, 0)]
@@ -52,12 +67,6 @@ dayopen = df[df.time==datetime.time(9, 30, 0)]
 dayopen.reset_index(drop=True, inplace=True)
 dayclose.reset_index(drop=True, inplace=True)
 dayclose.sort_values(by='date')
-display(df, dayopen.head(), dayclose.head())
-df0 = df.copy()
-
-# df['hour'] = pd.to_datetime(df['time'], format='%H:%M:%S').dt.hour
-# df['minute'] = pd.to_datetime(df['time'], format='%H:%M:%S').dt.minute
-
 
 ### now i wanna do feature engineering for all assets 
 
@@ -69,21 +78,19 @@ for asset in asset_list:
     df['s_' + asset + '_ret_1prd'] = (100*(df[asset]/df[asset].shift(1)-1)).shift(1)
     df['s_' + asset + '_ret_2prd'] = (100*(df[asset]/df[asset].shift(2)-1)).shift(1)
     df['s_' + asset + '_ret_4prd'] = (100*(df[asset]/df[asset].shift(4)-1)).shift(1)
-    display(df.shape, df.head(5))
+    # display(df.shape, df.head(5))
 
     df.loc[df.time < datetime.time(9, 32, 0), 's_' + asset + '_1prd'] = np.nan
     df.loc[df.time < datetime.time(9, 33, 0), 's_' + asset + '_2prd'] = np.nan
     df.loc[df.time < datetime.time(9, 35, 0), 's_' + asset + '_4prd'] = np.nan
 
     dayopen.rename(columns={asset:asset+'_open'}, inplace=True)
-    dayopen.head()
+    # dayopen.head()
     dayclose.rename(columns={asset:asset+'_close'}, inplace=True)
     dayclose_l1 = dayclose.copy()
     dayclose_l2 = dayclose.copy()
     dayclose_l1[asset+'_close_l1'] = dayclose_l1[asset+'_close'].shift(1)
     dayclose_l2[asset+'_close_l2'] = dayclose_l2[asset+'_close'].shift(2)
-
-    # display(dayclose_l1.head(), dayclose_l2.head())
 
     df = pd.merge(df, dayopen[['date', asset + '_open']], on=['date'], how='left')
     df = pd.merge(df, dayclose_l1[['date', asset + '_close_l1']], on=['date'], how='left')
@@ -96,91 +103,49 @@ for asset in asset_list:
     cols_todrop = [x for x in list(df.columns) if asset in x and 'ret' not in x]
     df.drop(columns = cols_todrop, inplace=True)
 
-display(time.time() - time0, df.head())
+### do prediction ###
 
-### do modeling ###
+model_path = '/home/jupyter/project_repos/spg_stocks/spg_stocks/stocks-app/en_model.pkl'
+trained_model = joblib.load(open(model_path, "rb"))
 
-t_df = df.copy()
-t_df.rename(columns={'VTHR_ret':'target'}, inplace=True)
-t_df.drop(columns = ['time', 'date', 'Spx_ret', 'Nasdaq_ret', 'Russel_ret', 'EEMA_ret', 'EEM_ret', 'EMXC_ret', 'VXUS_ret'], 
-          inplace=True,
-          errors = 'ignore')
-t_df
+this_day = df.loc[df.date == df.date.max()]
+print(f'{this_day.shape[0]} observations this day')
+X = this_day.copy()
+X.drop(columns = ['Datetime',
+                  'time', 
+                  'date', 
+                  'Spx_ret', 
+                  'Nasdaq_ret', 
+                  'Russel_ret', 
+                  'EEMA_ret', 
+                  'EEM_ret', 
+                  'EMXC_ret', 
+                  'VXUS_ret', 
+                  'VTHR_ret'], 
+                  inplace=True,
+                  errors = 'ignore')
 
-t_df = t_df.dropna()
-t_df.info()
+if(X.count().sum() < X.shape[1]):
+    print(f'''There are {X.shape[1] - X.count().sum()} missing values. 
+          There will be an error''')
 
+y = this_day.VTHR_ret
+y_hat = trained_model.predict(X)
 
-y = t_df.pop('target')
-X = t_df
+model_rmse = mean_squared_error(y, y_hat)
+constant_rmse = mean_squared_error(y, np.zeros(len(y)))
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=int(0.2*X.shape[0]))
-display(X_train.shape, X_test.shape, y_train.shape, X_train.head())
+performance = pd.DataFrame([[100*(r2_score(y, y_hat)), model_rmse, constant_rmse, 100*(1-model_rmse/constant_rmse)]], 
+                           columns = ['R2', 'model_rmse', 'constant_rmse', 'rmse_improvement'])
 
-time1 = time.time()
+file_name = 'm1_performance_' + \
+str(df.date.max().year) + \
+str(df.date.max().month) + \
+str(df.date.max().day) + \
+'_pull_time_' + \
+now_time + \
+'.csv'
+performance.to_csv('gs://pmykola-streaming-projects/spg-stocks/artifacts' + '/' + file_name)
 
-# xgbm = XGBRegressor()
-# parameters = {'eta':[0.03, 0.04, 0.05, 0.06, 0.07], 
-#               'max_depth':[2, 3],
-#              'subsample':[0.6, 0.8],
-#              'colsample_bytree':[0.6, 0.8]}
-# xgbgs = GridSearchCV(xgbm, parameters, cv=2)
-# xgbgs.fit(X_train, y_train)
-# print(xgbgs.best_params_)
-# xgbt = XGBRegressor(**xgbgs.best_params_)
-xgbt = XGBRegressor(n_estimators=500, eta=0.005, max_depth=2, subsample=0.6, colsample_bytree=0.5)
-xgbt.fit(X_train, y_train)
-
-enm = ElasticNet()
-parameters = {'alpha':[0.0005, 0.001, 0.002, 0.003, 0.005], 
-              'l1_ratio':[0, 0.02, 0.05, 0.1, 0.25, 0.5, 1]}
-enmgs = GridSearchCV(enm, parameters, scoring='r2', cv=4)
-enmgs.fit(X_train, y_train)
-print(enmgs.best_params_)
-enmt = XGBRegressor(**enmgs.best_params_)
-enmt.fit(X_train, y_train)
-
-print('In sample, xgb: ', r2_score(y_train, xgbt.predict(X_train)))
-print('Out of sample, xgb: ', r2_score(y_test, xgbt.predict(X_test)))
-
-print('In sample, ElasticNet: ', r2_score(y_train, enmgs.predict(X_train)))
-print('Out of sample, ElasticNet: ', r2_score(y_test, enmgs.predict(X_test)))
-
-print('Total time: ', time.time()-time0)
-
-
-# explainerxgbc = shap.TreeExplainer(xgbt)
-explainerxgbc = shap.Explainer(enmt)
-
-shap_values_XGBoost_test = explainerxgbc.shap_values(X_test)
-shap_values_XGBoost_train = explainerxgbc.shap_values(X_train)
-
-vals = np.abs(shap_values_XGBoost_test).mean(0)
-feature_names = X_test.columns
-feature_importance = pd.DataFrame(list(zip(feature_names, vals)),
-                                 columns=['col_name','feature_importance_vals'])
-feature_importance.sort_values(by=['feature_importance_vals'],
-                              ascending=False, inplace=True)
-
-shap.summary_plot(shap_values_XGBoost_test, X_test, plot_type="bar", plot_size=(6,6), max_display=20)
-shap.summary_plot(shap_values_XGBoost_train, X_train,plot_type="dot", plot_size=(6,6), max_display=20)
-
-# xgboost sucks here
-# takeaway is that to do regression with xgboost I need more observations than to do classification
-# 5,000 obs is definitely not enough for xgb regression, so it is not even worth trying.
-
-
-"""
-print(f'''minimum and maximum values in train sample are 
-{y_train.min()}, {y_train.max()}''')
-print(f'''minimum and maximum values in test sample are 
-{y_test.min()}, {y_test.max()}''')
-
-print(f'''predicted minimum and maximum values in train sample are 
-{enmt.predict(X_train).min()}, {enmt.predict(X_train).max()}''')
-print(f'''predicted minimum and maximum values in test sample are 
-{enmt.predict(X_test).min()}, {enmt.predict(X_test).max()}''')
-
-may want to add truncation to predicted values later
-"""
-
+print(f'''Success. File {file_name} created and uploaded. 
+Total time: {time.time()-time0} sec.''')
